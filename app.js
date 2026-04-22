b76fe8bb258299e7a59da682c195f3c9c5ac2091
trhanford <55467076+trhanford@users.noreply.github.com>
2026-04-22 10:30:44 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
- import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
 import { HOUSE_RULES } from "./house-rules.js";
 import {
   getFirestore,
